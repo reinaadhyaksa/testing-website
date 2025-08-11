582318c59a26f6a9e02ef8bc151c05ab4fac66bd
raichan <violetevargarde@gmail.com>
2025-08-11 07:55:54 +0700

    ayy

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,13 @@ const uploadRouter = require('./upload');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://testing-website-omega.vercel.app',
+        'http://localhost:5000'
+    ]
+}));
+
 app.use(express.json());
 
 app.set('supabase', supabase);
